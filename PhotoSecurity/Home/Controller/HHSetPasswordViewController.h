a750714b18ca69b8bb657dcf877883ea754ffcf6
Evan <huwen126@126.com>
2019-07-26 00:52:14 +0800

    *

---
+++ PhotoSecurity/Home/Controller/HHSetPasswordViewController.h
@@ -0,0 +1,17 @@
+//
+//  HHSetPasswordViewController.h
+//  PhotoSecurity
+//
+//  Created by Evan on 2019/7/25.
+//  Copyright © 2019 xiaopin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HHSetPasswordViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PhotoSecurity/Home/Controller/HHSetPasswordViewController.m
@@ -0,0 +1,23 @@
+//
+//  HHSetPasswordViewController.m
+//  PhotoSecurity
+//
+//  Created by Evan on 2019/7/25.
+//  Copyright © 2019 xiaopin. All rights reserved.
+//
+
+#import "HHSetPasswordViewController.h"
+
+@interface HHSetPasswordViewController ()
+
+@end
+
+@implementation HHSetPasswordViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+@end
